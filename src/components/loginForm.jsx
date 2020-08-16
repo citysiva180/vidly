d0cf9b505696d5cd13d9350cd6222445a0f28d7a
Sivarajan <citysiva180@gmail.com>
2020-08-17 05:29:41 +0530

    LoginForm_commit

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Input from "../components/input";
 
 class LoginForm extends Component {
   state = {
@@ -28,30 +29,20 @@ class LoginForm extends Component {
 
     return (
       <div>
-        <form onSubmit={this.handleSubmit} action="">
-          <div className="form-group">
-            <label htmlFor="username">User Name</label>
-            <input
-              value={account.username}
-              onChange={this.handleChange}
-              autoFocus
-              ref={this.username}
-              id="username"
-              name="user"
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              value={account.password}
-              onChange={this.handleChange}
-              id="password"
-              type="text"
-              className="form-control"
-            />
-          </div>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+          />
+          <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange}
+          />
+
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
